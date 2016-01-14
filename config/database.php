5ee8cb5887506b73ed9f58011f574c74096a4bfc
vevalentino24 <ve.febrian@gmail.com>
2016-01-14 15:43:40 +0700

    no change

--- config/database.php
+++ config/database.php
@@ -55,7 +55,7 @@ return [
         'mysql' => [
             'driver'    => 'mysql',
             'host'      => 'localhost',
-            'database'  => 'monev_dev',
+            'database'  => 'monev_kudus',
             'username'  => 'root',
             'password'  => '',
             'charset'   => 'utf8',
